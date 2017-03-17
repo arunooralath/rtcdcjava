689eb275010ec1421a93d8a242a0ed728c83ed3a
Erik De Rijcke <DeRijcke.Erik@gmail.com>
2017-03-17 23:27:00 +0100

    finish dtls unit test

--- src/test/java/com/github/zubnix/rtcdcjava/DTLSTransportFactoryTest.java
+++ src/test/java/com/github/zubnix/rtcdcjava/DTLSTransportFactoryTest.java
@@ -1,7 +1,6 @@
 package com.github.zubnix.rtcdcjava;
 
 
-import org.bouncycastle.crypto.tls.ByteQueueOutputStream;
 import org.bouncycastle.crypto.tls.DTLSTransport;
 import org.bouncycastle.crypto.tls.DatagramTransport;
 import org.junit.Assert;
@@ -12,15 +11,13 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
-import java.nio.ByteBuffer;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class DTLSTransportFactoryTest {
 
     @Test
-    public void testClientServer() throws IOException, InterruptedException {
+    public void testClientServer() throws IOException, InterruptedException, ExecutionException {
         //given
         //client & server connection
 
@@ -32,6 +29,7 @@ public class DTLSTransportFactoryTest {
         //a message is send from client to server & vice verse
 
         //server thread
+        final CompletableFuture<Void> serverCompletionState = new CompletableFuture<>();
         final Thread serverThread = new Thread(() -> {
             try {
                 //server connection
@@ -59,10 +57,11 @@ public class DTLSTransportFactoryTest {
                                                                                                          serverSocket.setSoTimeout(waitMillis);
                                                                                                          try {
                                                                                                              serverSocket.receive(datagramPacket);
+                                                                                                             System.out.println(datagramPacket.getLength());
                                                                                                              return datagramPacket.getLength();
                                                                                                          }
                                                                                                          catch (final SocketTimeoutException e) {
-                                                                                                             return 0;
+                                                                                                             return -1;
                                                                                                          }
                                                                                                      }
 
@@ -70,11 +69,11 @@ public class DTLSTransportFactoryTest {
                                                                                                      public void send(final byte[] buf,
                                                                                                                       final int off,
                                                                                                                       final int len) throws IOException {
-                                                                                                         final InetAddress IPAddress = InetAddress.getByName("localhost");
+                                                                                                         final InetAddress IPAddress = InetAddress.getLoopbackAddress();
                                                                                                          serverSocket.send(new DatagramPacket(buf,
                                                                                                                                               len,
                                                                                                                                               IPAddress,
-                                                                                                                                              9876));
+                                                                                                                                              12345));
                                                                                                      }
 
                                                                                                      @Override
@@ -83,15 +82,25 @@ public class DTLSTransportFactoryTest {
                                                                                                      }
                                                                                                  });
 
-                final byte[] receiveBuffer = new byte[3];
-                serverTransport.receive(receiveBuffer,
-                                        0,
-                                        3,
-                                        100000);
+                final byte[] receiveBuffer = new byte[1500];
+                int          len           = -1;
+
+                int       retry    = 0;
+                final int maxRetry = 5;
+                //keep trying to receive a SYN
+                while ((len = serverTransport.receive(receiveBuffer,
+                                                      0,
+                                                      1500,
+                                                      500)) == -1 && retry < maxRetry) {
+                    retry++;
+                }
 
                 Assert.assertEquals("SYN",
-                                    new String(receiveBuffer));
+                                    new String(receiveBuffer,
+                                               0,
+                                               len));
 
+                //so we can send an ACK
                 final byte[] message = "ACK".getBytes();
                 serverTransport.send(message,
                                      0,
@@ -101,13 +110,17 @@ public class DTLSTransportFactoryTest {
                 e.printStackTrace();
                 throw new Error(e);
             }
+
+            serverCompletionState.complete(null);
         });
+        serverThread.setUncaughtExceptionHandler((t, e) -> serverCompletionState.completeExceptionally(e));
         serverThread.start();
 
+        final CompletableFuture<Void> clientCompletionState = new CompletableFuture<>();
         final Thread clientThread = new Thread(() -> {
             try {
                 //client connection
-                final DatagramSocket clientSocket = new DatagramSocket();
+                final DatagramSocket clientSocket = new DatagramSocket(12345);
 
                 final DTLSTransport clientTransport = dtlsTransportFactory.createClientTransport(new DatagramTransport() {
                     @Override
@@ -133,7 +146,7 @@ public class DTLSTransportFactoryTest {
                             return datagramPacket.getLength();
                         }
                         catch (final SocketTimeoutException e) {
-                            return 0;
+                            return -1;
                         }
                     }
 
@@ -141,7 +154,7 @@ public class DTLSTransportFactoryTest {
                     public void send(final byte[] buf,
                                      final int off,
                                      final int len) throws IOException {
-                        final InetAddress IPAddress = InetAddress.getByName("localhost");
+                        final InetAddress IPAddress = InetAddress.getLoopbackAddress();
                         clientSocket.send(new DatagramPacket(buf,
                                                              len,
                                                              IPAddress,
@@ -154,25 +167,36 @@ public class DTLSTransportFactoryTest {
                     }
                 });
 
-                final byte[] message = "SYN".getBytes();
-                clientTransport.send(message,
-                                     0,
-                                     message.length);
 
-                final byte[] receiveBuffer = new byte[10];
-                clientTransport.receive(receiveBuffer,
-                                        0,
-                                        3,
-                                        100000);
-                Assert.assertEquals("ACK",
-                                    new String(receiveBuffer));
+                //keep sending SYN
+                int       retry    = 0;
+                final int maxRetry = 5;
+                do {
+                    retry++;
+                    final byte[] message = "SYN".getBytes();
+                    clientTransport.send(message,
+                                         0,
+                                         message.length);
+                    try {
+                        Thread.sleep(500);
+                    }
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    //until we receive an ACK
+                } while (!receiveACK(clientTransport) && retry <= maxRetry);
+
 
             }
             catch (final IOException e) {
                 e.printStackTrace();
                 throw new Error(e);
             }
+
+            clientCompletionState.complete(null);
         });
+
+        clientThread.setUncaughtExceptionHandler((t, e) -> clientCompletionState.completeExceptionally(e));
         clientThread.start();
 
         //then
@@ -180,5 +204,28 @@ public class DTLSTransportFactoryTest {
         clientThread.join();
         serverThread.join();
 
+        clientCompletionState.get();
+        serverCompletionState.get();
+    }
+
+    private boolean receiveACK(final DTLSTransport clientTransport) throws IOException {
+
+        final byte[] receiveBuffer = new byte[1500];
+
+        final int len;
+        if ((len = clientTransport.receive(receiveBuffer,
+                                           0,
+                                           1500,
+                                           1000)) != -1) {
+            Assert.assertEquals("ACK",
+                                new String(receiveBuffer,
+                                           0,
+                                           len));
+
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
